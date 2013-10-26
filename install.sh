e90309f2009107687abeddc37280d3c522935d12
mtgreenway <mgreenway@uchicago.edu>
2013-10-25 22:59:21 -0500

    grabs pathogen

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 for i in $(ls -A)
 do
-    if [[ $i == "install.sh" || $i == ".git" ]]
+    if [[ $i == "install.sh" || $i == ".git" || $i == ".gitignore" ]]
     then
         continue
     fi
     ln -s $(pwd)/$i ~/$i
 done
+
+mkdir ~/.vim && git clone https://github.com/tpope/vim-pathogen.git
+cp -r vim-pathogen/autoload ~/.vim
