be1ad798dc26395442468ef1ff2ec9f4feaa0474
mtgreenway <mgreenway@uchicago.edu>
2013-11-25 20:30:34 -0600

    get syntastic and pylint

--- install.sh
+++ install.sh
@@ -11,3 +11,7 @@ done
 
 mkdir ~/.vim && git clone https://github.com/tpope/vim-pathogen.git
 cp -r vim-pathogen/autoload ~/.vim
+mkdir ~/.vim/bundle
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+#sudo apt-get install pylint
+sudo easy_install pylint
