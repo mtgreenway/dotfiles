bcb2a1eb734f0604088fe33401160874f4654c43
Matt Greenway <mgreenway@uchicago.edu>
2013-11-25 20:42:31 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ do
     ln -s $(pwd)/$i ~/$i
 done
 
-mkdir ~/.vim && git clone https://github.com/tpope/vim-pathogen.git
-cp -r vim-pathogen/autoload ~/.vim
+mkdir ~/.vim && git clone https://github.com/tpope/vim-pathogen.git /tmp/vim-pathogen
+cp -r /tmp/vim-pathogen/autoload ~/.vim
 mkdir ~/.vim/bundle
 git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
 #sudo apt-get install pylint
